b2fd28b5af7d35368958c52740057272b4154090
thoroughProgrammer <isaiah.idonije55@gmail>
2024-04-09 20:10:11 +0100

    First Commit

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from main import settings
 
-
 urlpatterns = [
     path('django_admin/', admin.site.urls),
     path('', include('accounts.urls'))
